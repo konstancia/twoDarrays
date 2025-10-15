e2f639cd9c1637870b92188ce1cd01f82d58bfd5
Blueyeskon <konstancia_milova@hotmail.com>
2025-10-15 10:08:21 -0700

    const

--- Program.cs
+++ Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main()
     {
+        const string MENU_COORD = "1";
+        const string MENU_NUM   = "2";
+        const string MENU_USER  = "3";
+        const string MENU_EXIT  = "0";
+
+        const char CORNER = '+';
+        const char HLINE  = '-';
+        const char VLINE  = '|';
+
+        const int MIN_CELL_WIDTH = 1;
+        const int COLOR_BUCKETS  = 4;
+
         while (true)
         {
             Console.WriteLine("1) Coordinates grid");
@@ -11,68 +23,121 @@ class Program
             Console.WriteLine("3) User input grid");
             Console.WriteLine("0) Exit");
             Console.Write("Pick: ");
-            string mode = Console.ReadLine();
-            if (mode == "0") break;
+            string mode = Console.ReadLine() ?? "";
+
+            if (mode == MENU_EXIT)
+            {
+                break;
+            }
 
             Console.Write("Rows: ");
-            int rows = int.Parse(Console.ReadLine());
+            int rows = int.Parse(Console.ReadLine() ?? "0");
+
             Console.Write("Cols: ");
-            int cols = int.Parse(Console.ReadLine());
+            int cols = int.Parse(Console.ReadLine() ?? "0");
 
             string[,] grid = new string[rows, cols];
 
-            if (mode == "1")
+            if (mode == MENU_COORD)
             {
                 for (int r = 0; r < rows; r++)
+                {
                     for (int c = 0; c < cols; c++)
+                    {
                         grid[r, c] = r + "," + c;
+                    }
+                }
             }
-            else if (mode == "2")
+            else if (mode == MENU_NUM)
             {
                 int num = 0;
                 for (int r = 0; r < rows; r++)
-                    for (int c = 0; c < cols; c++)
-                        grid[r, c] = num++.ToString();
-            }
-            else if (mode == "3")
-            {
-                for (int r = 0; r < rows; r++)
+                {
                     for (int c = 0; c < cols; c++)
                     {
-                        Console.Write($"[{r},{c}]: ");
-                        grid[r, c] = Console.ReadLine();
+                        grid[r, c] = (num++).ToString();
                     }
+                }
+            }
+            else if (mode == MENU_USER)
+            {
+                Console.WriteLine("Fill the grid:");
+                for (int r = 0; r < rows; r++)
+                {
+                    for (int c = 0; c < cols; c++)
+                    {
+                        Console.Write("[" + r + "," + c + "]: ");
+                        grid[r, c] = Console.ReadLine() ?? "";
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice");
+                continue;
             }
 
-            int w = 1;
+            int w = MIN_CELL_WIDTH;
             foreach (var cell in grid)
-                if (cell.Length > w) w = cell.Length;
+            {
+                string s = cell ?? "";
+                if (s.Length > w)
+                {
+                    w = s.Length;
+                }
+            }
 
             for (int r = 0; r < rows; r++)
             {
                 for (int c = 0; c < cols; c++)
-                    Console.Write("+" + new string('-', w));
-                Console.WriteLine("+");
+                {
+                    Console.Write(CORNER);
+                    Console.Write(new string(HLINE, w));
+                }
+                Console.WriteLine(CORNER);
 
                 for (int c = 0; c < cols; c++)
                 {
-                    Console.Write("|");
-                    if (mode == "2")
+                    Console.Write(VLINE);
+                    if (mode == MENU_NUM)
                     {
                         int n = int.Parse(grid[r, c]);
-                        if (n % 4 == 0) Console.ForegroundColor = ConsoleColor.Red;
-                        else if (n % 4 == 1) Console.ForegroundColor = ConsoleColor.Green;
-                        else if (n % 4 == 2) Console.ForegroundColor = ConsoleColor.Blue;
-                        else Console.ForegroundColor = ConsoleColor.Yellow;
+                        int bucket = n % COLOR_BUCKETS;
+
+                        if (bucket == 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                        }
+                        else if (bucket == 1)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                        }
+                        else if (bucket == 2)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                        }
+
+                        Console.Write(grid[r, c].PadLeft(w));
+                        Console.ResetColor();
                     }
-                    Console.Write(grid[r, c].PadLeft(w));
-                    Console.ResetColor();
+                    else
+                    {
+                        Console.Write(grid[r, c].PadLeft(w));
+                    }
                 }
-                Console.WriteLine("|");
+                Console.WriteLine(VLINE);
             }
+
             for (int c = 0; c < cols; c++)
-                Console.Write("+" + new string('-', w));
-            Console.WriteLine("+");
+            {
+                Console.Write(CORNER);
+                Console.Write(new string(HLINE, w));
+            }
+            Console.WriteLine(CORNER);
         }
     }
 }
